1ff683890aca0ae78d055a83b60c44ab85a1e81e
Gregory Lindsey <gclindsey@gmail.com>
2020-02-13 17:39:57 -0800

    Add Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM neo4j:3.5.14
+MAINTAINER Gregory Lindsey <gclindsey@gmail.com>
---
+++ import_data.py
@@ -0,0 +1,6 @@
+from py2neo import Graph, Node, Relationship
+
+# Create GraphDB engine
+# Connect to database
+# Run query to create nodes and relationships
+# Close connection
